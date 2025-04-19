671ce3858ecb9e7d03a61b25deaf8a8eb470cdc9
Lucas Rodrigues <Lucasrodriguesgomesl53@gmail.com>
2025-04-19 12:40:13 -0300

    Cabeçalhos, parágrafos e campos de formulário

--- Lógica de Programacao e Algoritmos com JavaScript/Cp_2.Integração com HTML/ex2_1.js
+++ Lógica de Programacao e Algoritmos com JavaScript/Cp_2.Integração com HTML/ex2_1.js
@@ -5,7 +5,7 @@ const resp = document.querySelector("h3");
 //Cria  um "ouvinte" de evento, acionado quando o botão submit for clicado.
 frm.addEventListener("submit", (e) =>{
     const nome = frm.inNome.value; //obtém o nome digitado no form.
-    resp.innerText = "Olá" + nome //exibe a resposta do programa.
+    resp.innerText = ("Olá " + nome); //exibe a resposta do programa.
     e.preventDefault(); //evita envio do form.
 
 })
