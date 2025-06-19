a869a5d5079685632b38c115d5fd554ec92932b4
Lucas Rodrigues <Lucasrodriguesgomesl53@gmail.com>
2025-06-18 23:51:52 -0300

    Update4.6

--- Lógica de Programacao e Algoritmos com JavaScript/Cp_4.condicoes/js/ex4_6.js
+++ Lógica de Programacao e Algoritmos com JavaScript/Cp_4.condicoes/js/ex4_6.js
@@ -3,5 +3,14 @@ const resp100 = document.querySelector("#outNota100");
 const resp50 = document.querySelector("#outNota50");
 const resp10 = document.querySelector("#outNota10");
 
+frm.addEventListener("submit", (e) => { //"escuta" evento submit do form
+    e.preventDefault()   //evita envio do form
+    const saque = Number(frm.inSaque.value) //obtém valor do saque
+    if(saque % 10 ! = 0) //se sque não é multiplo de 10
+    alert("Valor inválido para notas disponíveis (R$ 10, 50, 100)")
+    frm.inSaque.focus()
+    return
+})
+
 
  
