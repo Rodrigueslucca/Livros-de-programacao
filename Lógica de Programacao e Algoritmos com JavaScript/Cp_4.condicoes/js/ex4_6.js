a21ad88edbb5aeebc941599b348a064cd1f37cb8
Lucas Rodrigues <Lucasrodriguesgomesl53@gmail.com>
2025-06-20 11:13:14 -0300

    Programa Caixa Eletrônico (html/js/css)
    Em um determinado momento do dia, apenas notas de 10, 50 e 100 estão disponíveis em um terminal de caixa eletrônico. Elaborar um programa que leia um valor de saque de um cliente, verifique sua validade(ou seja, se pode ser pago com as notas disponíveis) e informe o número de notas de 100, 50 e 10 necessárias para pagar esse saque.

--- Lógica de Programacao e Algoritmos com JavaScript/Cp_4.condicoes/js/ex4_6.js
+++ Lógica de Programacao e Algoritmos com JavaScript/Cp_4.condicoes/js/ex4_6.js
@@ -1,15 +1,36 @@
 const frm = document.querySelector("form");
-const resp100 = document.querySelector("#outNota100");
-const resp50 = document.querySelector("#outNota50");
-const resp10 = document.querySelector("#outNota10");
+const nota100 = document.querySelector("#outNota100");
+const nota50 = document.querySelector("#outNota50");
+const nota10 = document.querySelector("#outNota10");
 
 frm.addEventListener("submit", (e) => { //"escuta" evento submit do form
-    e.preventDefault()   //evita envio do form
-    const saque = Number(frm.inSaque.value) //obtém valor do saque
-    if(saque % 10 ! = 0) //se sque não é multiplo de 10
-    alert("Valor inválido para notas disponíveis (R$ 10, 50, 100)")
-    frm.inSaque.focus()
-    return
+    e.preventDefault();   //evita envio do form
+    const saque = Number(frm.inSaque.value); //obtém valor do saque
+    if(saque % 10 != 0){ //se saque não é multiplo de 10
+    alert("Valor inválido para notas disponíveis (R$ 10, 50, 100)");
+    frm.inSaque.focus();
+    return;
+}
+const notasCem = Math.floor(saque/100)//calcula notas de 100
+let resto = saque % 100 //quanto sobra para pagar
+const notasCinquenta = Math.floor(resto / 50); //calcula notas de 50
+resto = resto % 50 //quanto ainda sobra
+const notasDez = Math.floor(resto / 10); // calcula notas de 10
+if(notasCem > 0 ){  //exibe as notas se houver
+    nota100.innerText = "Notas de R$ 100: " +notasCem;
+}
+if (notasCinquenta > 0){
+    nota50.innerText = "Notas de R$ 50: " +notasCinquenta;
+}else{
+    nota50.innerText = ""; //limpa o campo se não houver notas de 50
+}
+if (notasDez > 0){
+ nota10.innerText = "Notas de R$ 10: " +notasDez;
+}else{
+    nota10.innerText = ""; //limpa o campo se não houver notas de 10
+
+}
+
 })
 
 
