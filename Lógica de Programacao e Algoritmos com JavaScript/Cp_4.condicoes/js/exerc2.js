a7d6d24879d97e8ba0d6ee6c5b7525625ea2c8ae
Lucas Rodrigues <Lucasrodriguesgomesl53@gmail.com>
2025-11-04 08:27:08 -0300

    Programa Verifica Velocidade
    Elaborar um programa que leia um número. Informe se ele é par ou Impar. Faça com o if..else..tradicional, e após, tente criar a condição com o operador ternário.

--- Lógica de Programacao e Algoritmos com JavaScript/Cp_4.condicoes/js/exerc2.js
+++ Lógica de Programacao e Algoritmos com JavaScript/Cp_4.condicoes/js/exerc2.js
@@ -5,16 +5,23 @@ frm.addEventListener("submit", (e)=>{
     e.preventDefault(); //evita envio do form
     
     //obtém a velocidade permitida do input
-    const velocidadePermitida = Number(frm.inPermitida.value); 
-    const velocidadeCondutor = Number(frm.inCondutor.value);
+    const velocidadePermitida = Number(frm.velPermitida.value); 
+    const velocidadeCondutor = Number(frm.velCondutor.value);
 
     //verifica se a velocidade do condutror é maior que a permitida
-    if(velocidadeCondutor <= velocidadePermitida){
+    /*if(velocidadeCondutor <= velocidadePermitida){
         resp.innerText = "Situação: Sem Multa"
     }else if(velocidadeCondutor <= velocidadePermitida * 1.2){
         resp.innerText = "Situação: Multa Leve"
     }else {
         resp.innerText = "Situação: Multa Grave"
     }
+     */
+    //mesma situação  com operador ternário
+    const situacao = (velocidadeCondutor <= velocidadePermitida) ? "Sem Multa" :
+                    (velocidadeCondutor <= velocidadePermitida * 1.2) ? "Multa Leve" :
+                    "Multa Grave";
+    resp.innerText = `Situação: ${situacao}`;
 
-});
+}
+);
