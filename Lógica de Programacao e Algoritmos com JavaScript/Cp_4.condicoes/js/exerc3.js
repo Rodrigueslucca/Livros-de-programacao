1d0713cddc68b0c87595c0700f8df63099e51990
Lucas Rodrigues <Lucasrodriguesgomesl53@gmail.com>
2025-11-15 00:20:02 -0300

    concluído
    Elaborar um programa para simular um parquímetro, o qual leia o valor de moedas depositado em um terminal de estacionamento rotativo. O programa deve informar o tempo de permanência do veículo no local e o troco(se existir). Se o valor for inferior ao tempo mínimo, exiba a mensagem: "Valor Insuficiente". valores da tabela: 1,00 = 30min/ 1,75 = 60min / 3,00 = 120min

--- Lógica de Programacao e Algoritmos com JavaScript/Cp_4.condicoes/js/exerc3.js
+++ Lógica de Programacao e Algoritmos com JavaScript/Cp_4.condicoes/js/exerc3.js
@@ -8,17 +8,21 @@ frm.addEventListener("submit", (e)=>{
 
     //obtém os valores dos inputs
     let num1 = Number(frm.valormoeda.value).toFixed(2);
-    let troco = Number();
+    let troco = Number().toFixed(2);
    
- if(num1 = 1.00){
+ if(num1 == 1.00){
     resp.innerText = `Tempo: 30 minutos`;
-    resp2.innerText = `Troco: R$ 0.00`;
- 
-}else if(num1 = 2.00){
+    resp2.innerText= " " 
+}else if(num1 ==  2.00){
     troco = num1 - 1.75;
     resp.innerText= "Tempo: 60 minutos";
-    resp2.innerText= "Troco: R$ "+ troco.toFixed(2); 
-} 
+    resp2.innerText= "Troco: R$ "+ troco
+}else if(num1 == 3.00){
+    resp.innerText= "Tempo: 120 minutos";
+    resp2.innerText= " "
+}else{
+    resp.innerText= "Valor Insuficiente";
+}
 
 
     
