a9613db8def4af1879b97079e2b5f09fc4878537
Lucas Rodrigues <Lucasrodriguesgomesl53@gmail.com>
2025-11-19 09:28:22 -0300

    Update ex5_2.js

--- Lógica de Programacao e Algoritmos com JavaScript/Cp_5.repeticoes/js/ex5_2.js
+++ Lógica de Programacao e Algoritmos com JavaScript/Cp_5.repeticoes/js/ex5_2.js
@@ -5,9 +5,8 @@ frm.addEventListener("submit", (e)=>{ //"Escuta" o evento submit do form
     e.preventDefault() //evita o envio do form
     const numero = Number(frm.inNumero.value) //obtém o número
     let resposta = `Entre ${numero} e 1: `  //String para mostrar a resposta
-    for (let i = numero; i > 0; i = i - 1){
+    for (let i = numero; i > 0; i= i -1){
         resposta = resposta + i + ", " //resposta acumula números(e vírgulas)
     }
-    resp.innerText = resposta   //exibe a resposta
-
+    resp.innerText = resposta.slice (0, -2) //exibe a resposta, retirando a última vírgula
 })
