73113c7e46b40fef1497da4d811c503f22b74967
Lucas Rodrigues <Lucasrodriguesgomesl53@gmail.com>
2025-12-05 10:20:54 -0300

    exemplo 5.6
     Este programa recebe um número e informa se ele é ou não primo.

---
+++ Lógica de Programacao e Algoritmos com JavaScript/Cp_5.repeticoes/js/ex5_6.js
@@ -0,0 +1,20 @@
+const frm = document.querySelector("form") //obtém elemntos da página
+const resp = document.querySelector("h3")
+
+frm.addEventListener("submit", (e) => { //"escuta" evento submit do form
+    e.preventDefault() //evita envio do form
+    const num = Number(frm.inNumero.value)//obtém número informado
+    let numDivisores = 0  //declara e inicializa contador
+    for(let i = 1; i <= num; i++){ //percorre todos os possíveis divisores de num
+        if(num % i == 0){ //verifica se i(1, 2, 3...)é divisor do num
+            numDivisores++ //se é, incrementa contdor
+         }
+        }
+        if(numDivisores == 2) { //se possui apenas 2 divisores, é primo
+            resp.innerText = `${num} É primo`
+        }else {
+            resp.innerText = `${num} Não é primo`
+         }
+        })
+
+        /*Este programa recebe um número e informa se ele é ou não primo*/ 
