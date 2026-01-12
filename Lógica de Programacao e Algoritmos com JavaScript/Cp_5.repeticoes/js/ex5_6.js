a9227abef40b4e05e407ca8b5937e04291065c21
Lucas Rodrigues <Lucasrodriguesgomesl53@gmail.com>
2026-01-12 13:27:25 -0300

    Update ex5_6.js

--- Lógica de Programacao e Algoritmos com JavaScript/Cp_5.repeticoes/js/ex5_6.js
+++ Lógica de Programacao e Algoritmos com JavaScript/Cp_5.repeticoes/js/ex5_6.js
@@ -4,13 +4,15 @@ const resp = document.querySelector("h3")
 frm.addEventListener("submit", (e) => { //"escuta" evento submit do form
     e.preventDefault() //evita envio do form
     const num = Number(frm.inNumero.value)//obtém número informado
-    let numDivisores = 0  //declara e inicializa contador
-    for(let i = 1; i <= num; i++){ //percorre todos os possíveis divisores de num
-        if(num % i == 0){ //verifica se i(1, 2, 3...)é divisor do num
+    let temDivisor = 0  //declara e inicializa contador
+    for(let i = 2; i <= num / 2; i++){ //percorre todos os possíveis divisores de num
+        if(num % i == 0){ //se tem divisor
+            temDivisor = 1  //muda o flag
+            break // sai da repetição
             numDivisores++ //se é, incrementa contador
          }
         }
-        if(numDivisores == 2) { //se possui apenas 2 divisores, é primo
+        if(num > 1 && !temDivisor) { //se num > 1 e não possui divisor
             resp.innerText = `${num} É primo`
         }else {
             resp.innerText = `${num} Não é primo`
